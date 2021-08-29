1b94ddef3c122546cededa75e2384a25213c8d6b
unknown <you@example.com>
2021-08-29 20:40:31 +0800

    Templates complated

--- app/route.py
+++ app/route.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from app import app
 
 #两个路由
@@ -5,4 +7,16 @@ from app import app
 @app.route('/index')
 #1个视图函数
 def index():
-    return 'hello world'
+    user = {'username': '汪洋'}
+
+    posts = [
+        {
+            'author': {'username':'john'},
+            'body': 'Beautiful day in Portlad'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
