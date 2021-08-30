c7ce5947b69e4d699dd0326d98fa3b271477c569
unknown <you@example.com>
2021-08-30 22:51:26 +0800

    database finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 #print('等会谁（哪个包或模块）在使用我:{}'.format(__name__))
-print(app.config['SECRET_KEY'])
+#print(app.config['SECRET_KEY'])
 
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
 
-
-from app import route
+from app import route,models
