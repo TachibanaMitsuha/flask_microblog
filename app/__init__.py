6f9f380c3fcd0665e3356bbf16ce8ba0febf1e2e
unknown <you@example.com>
2021-08-30 21:04:11 +0800

    form finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+#print('等会谁（哪个包或模块）在使用我:{}'.format(__name__))
+print(app.config['SECRET_KEY'])
 
-print('等会谁（哪个包或模块）在使用我:{}'.format(__name__))
+
 
 from app import route
