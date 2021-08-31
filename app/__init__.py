1bfc57d6658076216eb67a21ea8518c956a4b13f
unknown <you@example.com>
2021-08-31 23:36:27 +0800

    user_login_register_finished

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +12,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)#数据库对象
 migrate = Migrate(app, db)#迁移引擎对象
 
+login = LoginManager(app)
+login.login_view = 'login'
 from app import route,models
